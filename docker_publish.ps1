b7e4d4dd59386cd0eaacfd3b14e92797fa3cc9f0
David Horvath <david.horvath@amanox.ch>
2022-03-07 17:28:27 +0100

    introduce versioned image tagging

--- docker_publish.ps1
+++ docker_publish.ps1
@@ -1,3 +1,6 @@
+$TAG="1.1.0"
 docker login -u systematicguy
 docker build -t systematicguy/auto-rsync:latest .
+docker tag systematicguy/auto-rsync:latest systematicguy/auto-rsync:$TAG
 docker push systematicguy/auto-rsync:latest
+docker push systematicguy/auto-rsync:$TAG
